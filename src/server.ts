ea2479d1997af84d4f0ee72150cdf059b4271c6c
mestretools <mestrematuzalem@gmail.com>
2023-05-17 18:50:27 -0300

    base criada

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+dotenv.config();
+
+const server = express();
+
+// configuracao do template
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+// configuracao da pasta
+server.use(express.static(path.join(__dirname, '../public')));
+
+// configuracao das rota
+server.use(mainRoutes);
+
+// configuracao de rota nao encontrada
+server.use((req, res) => {
+    res.status(404).send('Página não encontrada.');
+});
+
+// configuração do server
+server.listen(process.env.PORT);
